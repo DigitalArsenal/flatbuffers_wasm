710936c1d550a81e645f47450265b23124bf1785
tj <tj@digitalarsenal.io>
2022-11-15 10:32:17 -0500

    removed error message

--- scripts/build.emscripten.flatbuffers.sh
+++ scripts/build.emscripten.flatbuffers.sh
@@ -11,6 +11,7 @@ sed -i "s/\"read_/\"read/" src/idl_gen_ts.cpp
 sed -i "s/mkdir.*;/return;/" src/util.cpp
 sed -i "s/if (binary) {/if (false) {/" src/util.cpp
 sed -i "s/if (DirExists(name)) return false;//" src/util.cpp
+sed -i "s/!IsLowerSnakeCase(name)/false/" src/idl_parser.cpp
 #cp ../../scripts/replacements/idl_parser.cpp src/idl_parser.cpp
 #cp ../../scripts/replacements/bfbs_gen.h src/bfbs_gen.h
 emcmake cmake . -DFLATBUFFERS_NO_ABSOLUTE_PATH_RESOLUTION=ON -DFLATBUFFERS_BUILD_LEGACY=OFF -DFLATBUFFERS_BUILD_SHAREDLIB=OFF -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_CXX_FLAGS="-v -g -Qunused-arguments -fno-exceptions -Os -s MODULARIZE=1 -s SINGLE_FILE=1 -s FORCE_FILESYSTEM=1"
