e2e82fef75fa193b832f4b9a565d4a3799696428
tj <tj@digitalarsenal.io>
2022-12-23 08:26:43 -0500

    restored

--- scripts/build.emscripten.flatbuffers.sh
+++ scripts/build.emscripten.flatbuffers.sh
@@ -9,7 +9,7 @@ sed -i "s/#ifdef FLATBUFFERS_NO_ABSOLUTE_PATH_RESOLUTION/#if 1/" src/util.cpp
 #sed -i "s/if.*kKeep)//" src/util.cpp
 cp ../../scripts/replacements/util.cpp src/util.cpp
 sed -i "s/\"read_/\"read/" src/idl_gen_ts.cpp
-#sed -i "s/kDasher/kKeep/" src/idl_gen_ts.cpp
+sed -i "s/kDasher/kKeep/" src/idl_gen_ts.cpp
 sed -i "s/mkdir.*;/return;/" src/util.cpp
 sed -i "s/if (binary) {/if (false) {/" src/util.cpp
 sed -i "s/if (DirExists(name)) return false;//" src/util.cpp
